f9352e415aed35fc5cb5e2a5aed195afa0e0331d
samuelkenji <sa.kgs10@gmail.com>
2020-03-27 11:03:10 -0300

    Implemetação dos testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express'); 
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -26,5 +27,6 @@ app.use(express.json());
 */
 
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
